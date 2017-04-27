7d9557a2ae2ef1f3fe529ba2aecf95c8aa07cec4
d陈志宏 <zhchen@coremail.cn>
2017-04-27 14:42:18 +0800

    增加transfer控件

--- src/components/transfer/Transfer.vue
+++ src/components/transfer/Transfer.vue
@@ -2,32 +2,49 @@
   <div class="transfer">
     <div class="transfer-list">
       <div class="transfer-list-header">
-        <v-checkbox>{{dataTotal}} items</v-checkbox>
+        <span>{{dataTotal}} items</span>
+        <!-- <v-checkbox></v-checkbox> -->
         <span class="title">{{titles[0]}}</span>
       </div>
-      <div class="transfer-list-body">
-        <ul class="transfer-list-content">
-          <li class="transfer-list-content-item" v-for="i in defaultDatas" :id="$index">
-            <v-checkbox :disabled="i.disabled" :default-checked.sync="i.value" :onchange="_addDataArray">{{i.label}}</v-checkbox>
+      <div :class="wrapClasses">
+        <div class="transfer-list-content-input" v-if="showFilter">
+          <input class="input" v-model="sourceInput" :placeholder="searchPlaceholder" />
+        </div>
+        <ul class="transfer-list-content" id="data">
+          <li class="transfer-list-content-item" v-for="i in dataSource | filter 'data' sourceInput" :id="$index" :title="i.title + ' - ' + i.description">
+            <v-checkbox :disabled="i.disabled" :id="i.key" :onchange="_handleSelectSource">{{i.title}}<span v-if="showDescription"> - {{i.description}}</span></v-checkbox>
           </li>
         </ul>
+      </div>
+      <div class="transfer-list-footer" v-if="showReload">
+        <v-button type="default" size="xsmall" @click="targetKeys = defaultTargetKeys">Reload</v-button>
       </div>
     </div>
     <div class="transfer-operation">
-      <v-button type="default" :disabled="dataArray.length = 0" size="small"><v-icon type="left"></v-icon></v-button>
-      <v-button type="default" :disabled="targetArray.length = 0 " size="small"><v-icon type="right"></v-icon></v-button>
+      <v-button type="primary" size="small" @click="_handleChange('left', targetSelectedKeys)" v-if="targetSelectedKeys.length !== 0"><v-icon type="left"></v-icon>{{operations[0]}}</v-button>
+      <v-button type="default" :disabled="true" size="small" v-else><v-icon type="left"></v-icon>{{operations[0]}}</v-button>
+      <v-button type="primary" size="small" @click="_handleChange('right', sourceSelectedKeys)" v-if="sourceSelectedKeys.length !== 0">{{operations[1]}}<v-icon type="right"></v-icon></v-button>
+      <v-button type="default" :disabled="true" size="small" v-else>{{operations[1]}}<v-icon type="right"></v-icon></v-button>
     </div>
     <div class="transfer-list">
       <div class="transfer-list-header">
-        <v-checkbox>{{targetTotal}} items</v-checkbox>
+        <span>{{targetTotal}} items</span>
+        <!-- <v-checkbox>{{targetTotal}} items</v-checkbox> -->
         <span class="title">{{titles[1]}}</span>
       </div>
-      <div class="transfer-list-body">
-        <ul class="transfer-list-content">
-          <li class="transfer-list-content-item" v-for="i in target" :id="$index">
-            <v-checkbox :disabled="i.disabled" :default-checked="i.value">{{i}}</v-checkbox>
+      <div :class="wrapClasses">
+        <div class="transfer-list-content-input" v-if="showFilter">
+          <input class="input" v-model="targetInput" :placeholder="searchPlaceholder" />
+        </div>
+        <ul class="transfer-list-content" id="target">
+          <li class="transfer-list-content-item" v-for="i in dataSource | filter 'target' targetInput" :id="$index" :title="i.title + ' - ' + i.description">
+            <v-checkbox :disabled="i.disabled" :id="i.key" :onchange="_handleSelectTarget">{{i.title}}<span v-if="showDescription"> - {{i.description}}</span></v-checkbox>
           </li>
+          <li class="transfer-list-content-item" v-if="targetKeys.length === 0">{{notFoundContent}}</li>
         </ul>
+      </div>
+      <div class="transfer-list-footer" v-if="showReload">
+        <v-button type="default" size="xsmall" @click="targetKeys = []">Clear all</v-button>
       </div>
     </div>
   </div>
@@ -37,43 +54,134 @@ import {defaultProps} from '../../views/utils/props'
 import vCheckbox from '../checkbox'
 import vButton from '../button'
 import vIcon from '../iconfont'
-// import cx from 'classnames'
+import cx from 'classnames'
+function sortNumber (a, b) {
+  return a - b
+}
+
 export default {
   props: defaultProps({
     prefixCls: 'transfer',
     titles: [],
-    datas: []
+    dataSource: [],
+    targetKeys: [],
+    showFilter: false,
+    showDescription: false,
+    showReload: false,
+    operations: [],
+    searchPlaceholder: 'Search',
+    notFoundContent: 'Not Result',
+    onchange: () => {}
   }),
   data () {
     return {
-      defaultDatas: this.datas,
+      sourceInput: '',
+      targetInput: '',
       dataTotal: 0,
-      target: [],
       targetTotal: 0,
-      dataArray: [],
-      targetArray: []
+      sourceSelectedKeys: [],
+      targetSelectedKeys: [],
+      defaultTargetKeys: this.targetKeys
     }
   },
-  ready () {
-    var self = this
-    self.defaultDatas.forEach(function(val, index) {
-      if (val.value) {
-        self.target.push(val.label)
-        self.defaultDatas.splice(index, 1)
+  computed: {
+    wrapClasses () {
+      return cx({
+        [`${this.prefixCls}-list-body`]: 1,
+        [`${this.prefixCls}-list-body-with-search`]: !!this.showFilter,
+        [`${this.prefixCls}-list-body-with-footer`]: !!this.showReload
+      })
+    }
+  },
+  filters: {
+    'filter' (val, index, inputValue) {
+      const target = []
+      var self = this
+      switch (index) {
+        case 'target':
+          val.forEach(function (val) {
+            if (self.targetKeys.includes(val.key)) {
+              if (inputValue && inputValue.toString().length > 0) {
+                if (val.title.indexOf(inputValue) > -1) {
+                  target.push(val)
+                }
+              } else {
+                target.push(val)
+              }
+            }
+          })
+          self.targetTotal = target.length
+          break
+        case 'data':
+          val.forEach(function (val) {
+            if (!self.targetKeys.includes(val.key)) {
+              if (inputValue && inputValue.toString().length > 0) {
+                if (val.title.indexOf(inputValue) > -1) {
+                  target.push(val)
+                }
+              } else {
+                target.push(val)
+              }
+            }
+          })
+          self.dataTotal = target.length
+          break
       }
-    })
-    self.dataTotal = self.defaultDatas.length
-    self.targetTotal = self.target.length
+      return target
+    }
   },
   methods: {
-    _addDataArray (e) {
+    _handleSelectSource (e) {
       var self = this
       if (e.target.checked) {
-        console.log(e.target.labels[0].innerText)
-        self.dataArray.push(e.target.labels[0].innerText)
+        this.sourceSelectedKeys.push(parseInt(e.target.labels[0].id))
+      } else {
+        self.sourceSelectedKeys.forEach(function (val, index) {
+          if (val === parseInt(e.target.labels[0].id)) {
+            self.sourceSelectedKeys.splice(index, 1)
+          }
+        })
+      }
+    },
+    _handleSelectTarget (e) {
+      var self = this
+      if (e.target.checked) {
+        this.targetSelectedKeys.push(parseInt(e.target.labels[0].id))
+      } else {
+        self.targetSelectedKeys.forEach(function (val, index) {
+          if (val === parseInt(e.target.labels[0].id)) {
+            self.targetSelectedKeys.splice(index, 1)
+          }
+        })
+      }
+    },
+    _handleChange (direction, moveKeys) {
+      switch (direction) {
+        case 'left':
+          let target = new Set(this.targetKeys)
+          let move = new Set(moveKeys)
+          let difference = Array.from(new Set(this.targetKeys.concat(moveKeys).filter(v => !target.has(v) || !move.has(v))))
+          this.targetKeys = difference.sort(sortNumber)
+          this.targetSelectedKeys = []
+          this.onchange()
+          break
+        case 'right':
+          let difference1 = Array.from(new Set(this.targetKeys.concat(moveKeys)))
+          this.targetKeys = difference1.sort(sortNumber)
+          this.sourceSelectedKeys = []
+          this.onchange()
+          break
       }
     }
   },
+  // watch: {
+  //   'targetSelectedKeys' (val) {
+  //     console.log('targetSelectedKeys:' + val)
+  //   },
+  //   'sourceSelectedKeys' (val) {
+  //     console.log('sourceSelectedKeys:' + val)
+  //   }
+  // },
   components: {
     vCheckbox,
     vButton,
--- src/views/components/transfer.vue
+++ src/views/components/transfer.vue
@@ -11,16 +11,68 @@
 
   选择一个或以上的选项后，点击对应的方向键，可以把选中的选项移动到另一栏。 其中，左边一栏为 source，右边一栏为 target，API 的设计也反应了这两个概念。
 
+  ## 如何使用
+
+  ```html
+  data () {
+    return {
+      titles: ['Source', 'Target'],
+      operations: ['To Left', 'To Right'],
+      datas: [],
+      targets: []
+    }
+  },
+  created () {
+    var self = this
+    const mockData = []
+    for (let i = 0; i < 20; i++) {
+      mockData.push({
+        key: i,
+        title: `content${i + 1}`,
+        description: `description of content${i + 1}`,
+        disabled: i % 3 < 1
+      })
+    }
+    self.datas = mockData
+    const targetKeys = mockData
+      .filter(item => +item.key % 3 > 1)
+      .map(item => item.key)
+    self.targets = targetKeys
+  }
+  ```
+
   ## API
 
-  | 参数       | 类型           | 默认值      | 说明         |
-  |------------|----------------|-------------|--------------|
+  | 参数       | 说明         | 类型           | 默认值      |
+  |------------|--------------|-------------|--------------|
+  | titles | 标题集合,顺序从左至右 | Array | undefined |
+  | operations | 操作文案集合,顺序从上至下 | Array | undefined |
+  | dataSource | 数据源，其中的数据将会被渲染到左边一栏中，`targetKeys`中指定的除外。 | Array | [] |
+  | targetKeys | 显示在右侧框数据的key集合 | Array | [] |
+  | onchange | 选项在两栏之间转移时的回调函数 | Function | () => {} |
+  | showFilter | 是否显示搜索框 | Boolean | false |
+  | showDescription | 是否显示备注 | Boolean | false |
+  | showReload | 是否显示底部，底部含`clear all`和`Reload`按钮用于重置数据 | Boolean | false |
+  | searchPlaceholder | 搜索框的默认值 | String | 'Search' |
+  | notFoundContent | 当列表为空时显示的内容 | String | 'Not Result' |
 
   ## 组件演示
 
   <demo>
     <example title="基本">
-      <v-transfer :titles="titles" :datas="datas"></v-transfer>
+      <v-transfer :titles="titles" :data-source="datas" :target-keys="targets"></v-transfer>
+    </example>
+    <example title="带搜索框">
+      <v-transfer :titles="titles" :data-source="datas" :target-keys="targets" :show-filter="true"></v-transfer>
+    </example>
+    <example title="显示备注">
+      <v-transfer :titles="titles" :data-source="datas" :target-keys="targets" :show-filter="true" :show-description="true"></v-transfer>
+    </example>
+    <example title="重置和清除按钮">
+      <v-transfer :titles="titles" :data-source="datas" :target-keys="targets" :show-filter="true" :show-reload="true"></v-transfer>
+    </example>
+    <example title="自定义文字">
+      <v-transfer :titles="titles" search-placeholder="搜索" :data-source="datas" :target-keys="targets" :show-filter="true" :operations="operations"></v-transfer>
     </example>
   </demo>
 </template>
@@ -30,48 +82,30 @@
     data () {
       return {
         titles: ['Source', 'Target'],
-        datas: [{
-          'value': true,
-          'label': 'content1'
-        }, {
-          'value': false,
-          'label': 'content2'
-        }, {
-          'value': true,
-          'label': 'content3',
-          disabled: true
-        }, {
-          'value': false,
-          'label': 'content4'
-        }, {
-          'value': false,
-          'label': 'content5'
-        }, {
-          'value': false,
-          'label': 'content6'
-        }, {
-          'value': false,
-          'label': 'content7'
-        }, {
-          'value': false,
-          'label': 'content8'
-        }, {
-          'value': false,
-          'label': 'content9'
-        }, {
-          'value': true,
-          'label': 'content10'
-        }, {
-          'value': false,
-          'label': 'content11'
-        }, {
-          'value': false,
-          'label': 'content12'
-        }]
+        operations: ['To Left', 'To Right'],
+        datas: [],
+        targets: []
       }
     },
     components: {
       vTransfer
+    },
+    created () {
+      var self = this
+      const mockData = []
+      for (let i = 0; i < 20; i++) {
+        mockData.push({
+          key: i,
+          title: `content${i + 1}`,
+          description: `description of content${i + 1}`,
+          disabled: i % 3 < 1
+        })
+      }
+      self.datas = mockData
+      const targetKeys = mockData
+        .filter(item => +item.key % 3 > 1)
+        .map(item => item.key)
+      self.targets = targetKeys
     }
   }
 </script>
